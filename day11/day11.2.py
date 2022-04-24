93d9f3dd437de73995ac979e1de47398aa8ecb88
PRAHLAD KUMAR <prahladkumar3741@gmail.com>
2022-04-24 21:01:37 +0545

    Dilation and Edge Detection

---
+++ day11/day11.2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('images/input.jpg',0)
+
+height, width = image.shape
+
+# Extract Sobel Edges
+sobel_x = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+sobel_y = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+
+cv2.imshow('Original', image)
+cv2.waitKey(0)
+cv2.imshow('Sobel X', sobel_x)
+cv2.waitKey(0)
+cv2.imshow('Sobel Y', sobel_y)
+cv2.waitKey(0)
+
+sobel_OR = cv2.bitwise_or(sobel_x, sobel_y)
+cv2.imshow('sobel_OR', sobel_OR)
+cv2.waitKey(0)
+
+laplacian = cv2.Laplacian(image, cv2.CV_64F)
+cv2.imshow('Laplacian', laplacian)
+cv2.waitKey(0)
+
+# Canny Edge Detection uses gradient values as thresholds
+# The first threshold gradient
+canny = cv2.Canny(image, 50, 120)
+cv2.imshow('Canny', canny)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
